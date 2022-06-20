416578d594d83a4bb190f555bc69415c9449a2bd
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-20 06:06:38 +0300

    commit tree 64f96e98caba21fadf3778a711495f1e7f47e76c

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Your Code Here */
 
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +22,75 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arr) {
+        return arr.map(creat)
+}
+
+function creat(i) {
+    return createEmployeeRecord(i)
+}
+
+
+
+function createTimeInEvent(stamp) {
+    let returnObj = {
+        type: "TimeIn",
+        hour: parseInt(stamp.slice(11,)),
+        date: stamp.slice(0,10)
+    }
+
+    this.timeInEvents.push(returnObj)
+
+    return this
+}
+
+function createTimeOutEvent(stamp) {
+
+    let returnObj = {
+        type: "TimeOut",
+        hour: parseInt(stamp.slice(11,)),
+        date: stamp.slice(0,10)
+    }
+
+    this.timeOutEvents.push(returnObj)
+
+    //console.log(this)
+    return this
+}
+
+function hoursWorkedOnDate(stamp) {
+    let start = this.timeInEvents.find(i => stamp === i.date)
+    let finish = this.timeOutEvents.find(i => stamp === i.date)
+    return (finish.hour-start.hour)/100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName (employees, name) {
+    let c =  employees.find(function(i) {
+        
+        return i.firstName === name
+    })
+    return c
+}
+
+function calculatePayroll(arr) {
+    return arr.reduce(addUp, 0)
+}
+
+function addUp (acc, i) {
+    return acc + allWagesFor.call(i)
+}
